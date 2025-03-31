f0e1601eda509a5e16bd5b5ff7bf3183ecc20866
Mitesh Shah <mitesh@miteshshah.com>
2025-03-31 20:18:18 +0530

    sent / received, rubber band selection boxes

--- canvas-01/README.md
+++ canvas-01/README.md
@@ -10,3 +10,9 @@ pnpm start
 ## Gif
 
 ![flow gif](./01-flow.gif)
+
+## Explore
+
+- entity component system
+  - pipelining arrays
+- simulate and draw 100,000,000 objects
--- canvas-01/src/main.js
+++ canvas-01/src/main.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("app");
 const ctx = canvas.getContext("2d");
 
 // Set display size (css pixels).
-var size = 1300;
+var size = 800;
 function setup() {
   canvas.style.width = size + "px";
   canvas.style.height = size + "px";
@@ -61,29 +61,42 @@ let blocks = [
     y: size/4,
     radius: size/30,
     color: "green",
+    type: "block",
+    sent: 0,
+    collected: 0,
   },
   {
     id: "Block B",
     x: size/3,
     y: 2*size/4,
     radius: size/30,
+    type: "block",
     color: "orange",
+    sent: 0,
+    collected: 0,
   },
   {
     id: "Block C",
     x: size/2,
     y: 3*size/4,
     radius: size/30,
+    type: "block",
     color: "red",
+    sent: 0,
+    collected: 0,
   },
   {
     id: "Block D",
     x: size - size/3,
     y: 2*size/4,
+    type: "block",
     radius: size/30,
     color: "pink",
+    sent: 0,
+    collected: 0,
   }
 ]
+
 function getBlock(id) {
   for (const block of blocks) {
     if (block.id == id) {
@@ -93,10 +106,29 @@ function getBlock(id) {
 }
 
 let parcels = [];
+let selecting = false;
+let selectedItems = [];
+let dragging= false;
+let dragPos = { x: 0, y: 0 };
+
+const drawText = (text, x, y, color="black") => {
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.font = "16px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText(text, x, y);
+  ctx.restore();
+}
 
 function drawBlock(block) {
   let {x, y, radius, color} = block;
   drawCircle(x, y, radius, color);
+  if (block.dragging) {
+    drawCircle(x, y, radius * 1.2, "black");
+  }
+  // drawText(block.id, x, y);
+  drawText(`⬆️${block.sent}`, x - radius, y - radius);
+  drawText(`⬇️${block.collected}`, x + radius, y - radius);
 }
 
 function drawGraph() {
@@ -110,15 +142,15 @@ function drawGraph() {
   }
 }
 
-
 for(let i = 0; i < 10; i++) {
   parcels.push({
     id: i,
     from: blocks[0].id,
     to: blocks[1].id,
     color: blocks[0].color,
+    type: "parcels",
     life: 0.05,
-    lifetime: 1.0,
+    lifetime: 2.0,
     speed: 1 + Math.random(),
   })
 }
@@ -147,19 +179,68 @@ function flow(dt) {
   for (let parcel of parcels) {
     let { life, lifetime, speed } = parcel;
     if (life > lifetime) {
+      let from = getBlock(parcel.from);
+      let to = getBlock(parcel.to);
       parcel.from = parcel.to;
       parcel.to = blocks[Math.floor(Math.random() * blocks.length)].id;
-      parcel.color = getBlock(parcel.from).color;
+      parcel.color = from.color
       parcel.life = 0;
+      from.sent += 1;
+      to.collected += 1;
     }
     parcel.life += dt * speed;
   }
 }
 
+function drawSelection(dt) {
+  if(selecting) {
+    console.log(`moving selection: ${dragPos.x}, ${dragPos.y} to ${selectionPos.x}, ${selectionPos.y}`);
+    ctx.save();
+    ctx.beginPath();
+    ctx.rect(dragPos.x, dragPos.y, (selectionPos.x - dragPos.x), (selectionPos.y - dragPos.y));
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.setLineDash([5, 5]);
+    ctx.stroke();
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fill();
+    ctx.restore();
+  }
+  if (selectedItems.length > 0) {
+    ctx.save();
+    ctx.beginPath();
+    xSorted = selectedItems.toSorted((a, b) => a.x - b.x)
+    ySorted = selectedItems.toSorted((a, b) => a.y - b.y)
+    ctx.rect(xSorted[0].x, ySorted[0].y, xSorted[xSorted.length - 1].x - xSorted[0].x, ySorted[ySorted.length - 1].y - ySorted[0].y)
+    ctx.strokeStyle = "cyan";
+    ctx.stroke();
+    ctx.restore();
+    ctx.fillStyle = "rgba(0, 0, 100, 0.1)";
+    ctx.fill();
+  }
+}
+
+function drawGrid() {
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "black";
+  ctx.setLineDash([10, 10]);
+  for (let i = 0; i < size; i += size / 10) {
+    ctx.moveTo(i, 0);
+    ctx.lineTo(i, size);
+    ctx.moveTo(0, i);
+    ctx.lineTo(size, i);
+  }
+  ctx.stroke();
+  ctx.restore();
+}
+
 function draw(dt) {
   setup();
   drawGraph();
-  
+  // drawGrid();
+  drawSelection(dt);
   for (let parcel of parcels) {
     drawParcel(parcel, dt);
   }
@@ -174,11 +255,10 @@ function ticker() {
   requestAnimationFrame(ticker)
 }
 
-let dragging= false;
-
 canvas.addEventListener("mousedown", (e) => {
   let x = e.offsetX;
   let y = e.offsetY;
+  console.log(`mousedown: ${x}, ${y}`);
   for (let block of blocks) {
     if (Math.abs(block.x - x) < block.radius && Math.abs(block.y - y) < block.radius) {
       dragging = true;
@@ -186,26 +266,67 @@ canvas.addEventListener("mousedown", (e) => {
       break;
     }
   }
+  if (!dragging) {
+    selecting = true;
+    dragPos = { x, y };
+    selectionPos = { x, y };
+  }
 });
 
 canvas.addEventListener("mousemove", (e) => {
   let x = e.offsetX;
   let y = e.offsetY;
+  if (selecting) {
+    selectionPos = { x, y };
+  }
+  // rubber band selection
+  /* else if (dragging && isBetween(dragPos.x, x, selectionPos.x) && isBetween(dragPos.y, y, selectionPos.y)) {
+   *   for(let item of selectedItems) {
+   *     item.x = x;
+   *     item.y = y;
+   *   }
+   *   selectionPos = {x, y};
+   * } */
   if (dragging) {
     for (let block of blocks) {
       if (block.dragging) {
-        block.x = x;
-        block.y = y;
+	block.x = x;
+	block.y = y;
       }
     }
   }
 });
 
-canvas.addEventListener("mouseup", (e) => {
-  dragging = false;
-  for (let block of blocks) {
-    block.dragging = false;
+const isBetween = (a, b, c) => {
+  return (a <= b && b <= c) || (c <= b && b <= a);
+}
+
+const mouseRelease = (e) => {
+  // dragPos = { x: 0, y: 0 };
+  // selectionPos = { x: 0, y: 0 };
+  selectedItems = [];
+  if (dragging) {
+    for (let block of blocks) {
+      block.dragging = false;
+    }
+    dragging = false;
   }
+  if (selecting) {
+    for (let block of blocks) {
+      if (isBetween(dragPos.x, block.x, selectionPos.x) && isBetween(dragPos.y, block.y, selectionPos.y)) {
+        selectedItems.push(block);
+      }
+    }
+    selecting = false;
+  }
+};
+
+canvas.addEventListener("mouseup", (e) => {
+  mouseRelease(e);
+});
+
+canvas.addEventListener("mouseleave", (e) => {
+  mouseRelease(e);
 });
 
 ticker();
